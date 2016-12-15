f1d8e8012787e0830f1c25f6d8dff72b83797267
Jeremie Dimino <jdimino@janestreet.com>
2016-12-15 11:20:46 +0000

    114.20+69

--- install.ml
+++
@@ -1,9 +0,0 @@
-#use "topfind";;
-#require "js-build-tools.oasis2opam_install";;
-
-open Oasis2opam_install;;
-
-generate ~package:"incr_select"
-  [ oasis_lib "incr_select"
-  ; file "META" ~section:"lib"
-  ]
