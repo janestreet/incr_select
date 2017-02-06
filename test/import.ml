efd9028b1e18bfc0ba2d6de0c694e972025048ed
Thomas Refis <thomas.refis@gmail.com>
2017-02-06 12:49:49 +0000

    114.30+114

--- test/import.ml
+++ test/import.ml
@@ -1,6 +1,6 @@
 include Expect_test_helpers_kernel
 module Incr = struct
-  module Incr = Incremental_kernel_debug.Std.Incremental.Make ()
+  module Incr = Incremental_kernel_debug.Incremental.Make ()
   include Incr
   module Select = Incr_select.Make (Incr)
 end
